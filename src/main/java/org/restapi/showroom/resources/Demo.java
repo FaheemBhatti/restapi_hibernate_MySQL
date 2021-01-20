7f3ad3d04bcc07ce91317128b769639d01b12146
FaheemBhatti <mefaheemazfar@gmail.com>
2021-01-20 13:00:04 +0100

    Adding context param

--- src/main/java/org/restapi/showroom/resources/Demo.java
+++ src/main/java/org/restapi/showroom/resources/Demo.java
@@ -5,18 +5,23 @@ import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
 
 @Path("/")
 public class Demo 
 {
+	
+	@Context 
+	private UriInfo uriInfo;
+	
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String useFullAnnotations(@HeaderParam("HeaderValue") String headerValue,
 			@CookieParam("CokieValue") String cokieValue)
 	{
-		return "Index Page having headerValue "+headerValue+ " Cookie Value "+ cokieValue;
+		return "Index Page having headerValue "+headerValue+ " Cookie Value "+ cokieValue
+				+" Complete path is " + uriInfo.getAbsolutePath();
 	}
-	
-
 }
